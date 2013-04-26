edc936b1cc721bcb44e5f315754cb4edc25ee98b
Taylor M. Maggert <spam@maggerts.com>
2013-04-26 10:52:21 -0600

    Finished chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,31 +1,35 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  
+describe "Static pages" do
+
   subject { page }
-  
-  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
-  
-  describe "Static Pages" do
-    describe "Home Page" do
-      before { visit root_path }
-      it { should have_selector('h1', text: "Welcome to the Sample App") }
-      it { should have_selector('title', text: full_title('')) }
-    end
-    describe "Help Page" do
-      before { visit help_path }
-      it { should have_selector('h2', text: "FAQ") }
-      it { should have_selector('title', text: full_title('Help')) }
-    end
-    describe "About Page" do
-      before { visit about_path }
-      it { should have_selector('h1', text: "About us") }
-      it { should have_selector('title', text: full_title('About')) }
-    end
-      describe "Contact Page" do
-        before { visit contact_path }
-        it { should have_selector('h1', text: "Contact Us") }
-        it { should have_selector('title', text: full_title('Contact')) }
-    end
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_selector('h1', text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
